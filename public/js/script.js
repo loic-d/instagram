6c139358c55d43d34acf1c82d170cdb7b031f584
Loïc de Laubier <loic-d@users.noreply.github.com>
2015-11-29 19:00:56 -0500

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,20 +1,9 @@
 $(document).ready(function(){
-
    var pathname = window.location.pathname.substring(1);
    $('.search-input').val(pathname);
-
    $('.search-form').on('submit', function(e){
        e.preventDefault();
        var search = $('.search-input').val();
        window.location.pathname = "/"+search;
    })
 });
-
-function isEmpty(object) {
-    for(var key in object) {
-        if(object.hasOwnProperty(key)){
-            return false;
-        }
-    }
-    return true;
-}
