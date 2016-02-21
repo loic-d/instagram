c4cf71bc085eef0f3d696993b25bed7dcf9d375c
Loic <loic.delaubier@gmail.com>
2016-02-21 12:07:12 -0500

    updates

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ A very simple Express application to extract pictures from Instagram by hastags
 
 * Clone the repo
 * Install dependencies
+* Add your CLIENT_ID and CLIENT_ID_SECRET in app.js
 * Run app.js
--- public/js/script.js
+++ public/js/script.js
@@ -2,10 +2,10 @@ $(document).ready(function(){
 
    var pathname = window.location.pathname.substring(1);
    $('.search-input').val(pathname);
-
    $('.search-form').on('submit', function(e){
        e.preventDefault();
        var search = $('.search-input').val();
        window.location.pathname = "/"+search;
    })
+
 });
